30926cfe09fdfee34c8923428590484b41ce4a76
wangyiyuelva <yiyuwang891120@gmail.com>
2024-04-18 22:24:53 +0100

    feature: add node-app for code submit

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.post('/submit-code', (req, res) => {
+  const code = req.body.code;
+  const filename = './mount/target.cpp';
+
+  fs.writeFile(filename, code, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error saving code');
+    }
+
+    console.log(`Code saved to: ${filename}`);
+    res.send('Code submitted successfully!');
+  });
+});
+
 
 app.listen(7001, () => {
   console.log('Server is running');
